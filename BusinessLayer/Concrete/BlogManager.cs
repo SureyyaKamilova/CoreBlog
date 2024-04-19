5e69871bc780cecbb91a99c2e522432b38cd8f04
user <user@DESKTOP-DDOO6UB>
2024-04-19 21:39:22 +0400

    Fix changes

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Interfaces;
+using DataAccessLayer.Interfaces;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		private readonly IBlog _blog;
+		public BlogManager(IBlog blog)
+		{
+			_blog = blog;
+		}
+
+		public void BlogAdd(Blog blog)
+		{
+			_blog.Insert(blog);
+		}
+
+		public void BlogDelete(Blog blog)
+		{
+			_blog.Delete(blog);
+		}
+
+		public void BlogUpdate(Blog blog)
+		{
+			_blog.Update(blog);
+		}
+
+		public List<Blog> GetAll()
+		{
+
+			return _blog.GetAll();
+		}
+
+		public Blog GetById(int id)
+		{
+
+			return _blog.GetById(id);
+		}
+	}
+}
---
+++ CoreDemo/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EFBlogRepository());
+        public IActionResult Index()
+        {
+            var values=blogManager.GetAll();
+            return View(values);
+        }
+    }
+}
