f0134c02f0d097626af4aa20c1c5b3e4896beab4
user <user@DESKTOP-DDOO6UB>
2024-04-21 19:49:04 +0400

    Create Register Controller

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Interfaces;
+using DataAccessLayer.Interfaces;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class WriterManager : IWriterService
+	{
+		private readonly IWriter _writer;
+		public WriterManager(IWriter writer)
+		{
+			_writer = writer;
+		}
+
+		public void WriterAdd(Writer writer)
+		{
+			_writer.Insert(writer);
+		}
+	}
+}
---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworks;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager writerManager = new WriterManager(new EFWriterRepository());
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Writer writer)
+		{
+			writer.WriterStatus = true;
+			writer.WriterAbout = "Writer About";
+			writerManager.WriterAdd(writer);
+
+			return RedirectToAction("Index","Blog");
+		}
+	}
+}
