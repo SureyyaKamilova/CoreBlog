d653793a07b6af93e5a9f39ea77f9ec6429fe26f
user <user@DESKTOP-DDOO6UB>
2024-04-21 20:36:12 +0400

    Add FluentValidation to Register View.

---
+++ BusinessLayer/ValidationRules/WriterValidation.cs
@@ -0,0 +1,32 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidation:AbstractValidator<Writer>
+    {
+        public WriterValidation()
+        {
+            RuleFor(x=>x.WriterName).NotEmpty()
+                .WithMessage("The author's name and surname section cannot be left blank.");
+
+            RuleFor(x => x.WriterName).MinimumLength(2)
+                .WithMessage("Enter at least 2 characters");
+
+            RuleFor(x=>x.WriterMail).NotEmpty()
+                .WithMessage("E-mail section cannot be left blank.");
+
+            RuleFor(x => x.WriterPassword).NotEmpty()
+                .WithMessage("Password section cannot be left blank.");
+
+            RuleFor(x => x.WriterPassword).MinimumLength(8)
+                .WithMessage("Enter at least 2 characters");
+
+        }
+    }
+}
--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,13 +1,16 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFrameworks;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class RegisterController : Controller
 	{
-		WriterManager writerManager = new WriterManager(new EFWriterRepository());
+	    WriterManager writerManager = new WriterManager(new EFWriterRepository());
+		
 		[HttpGet]
 		public IActionResult Index()
 		{
@@ -17,11 +20,27 @@ namespace CoreDemo.Controllers
 		[HttpPost]
 		public IActionResult Index(Writer writer)
 		{
-			writer.WriterStatus = true;
-			writer.WriterAbout = "Writer About";
-			writerManager.WriterAdd(writer);
+			WriterValidation writerValidations=new WriterValidation();
+			ValidationResult validationResult= writerValidations.Validate(writer);
 
-			return RedirectToAction("Index","Blog");
+			if(validationResult.IsValid)
+			{
+                writer.WriterStatus = true;
+                writer.WriterAbout = "Writer About";
+                writerManager.WriterAdd(writer);
+
+                return RedirectToAction("Index", "Blog");
+            }
+			else
+			{
+				foreach(var item in validationResult.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+
+			return View();
+			
 		}
 	}
 }
