6f31acc5555f42b7c310c78b145242fdc92c02b6
user <user@DESKTOP-DDOO6UB>
2024-05-01 12:36:05 +0400

    Fix problems and Dashboard

---
+++ CoreDemo/Controllers/DashBoardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
