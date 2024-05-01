cf453665ddb2952c09ac77d983f3a955436a0aa4
user <user@DESKTOP-DDOO6UB>
2024-05-01 17:44:00 +0400

    Add Statistics in DashBoard

--- CoreDemo/Controllers/DashBoardController.cs
+++ CoreDemo/Controllers/DashBoardController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
@@ -8,6 +10,10 @@ namespace CoreDemo.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context context=new Context();
+            ViewBag.v1=context.Blogs.Count().ToString();
+            ViewBag.yourblogs=context.Blogs.Where(x=>x.WriterId==1).Count().ToString();
+            ViewBag.category=context.Categories.Count().ToString() ;
             return View();
         }
     }
