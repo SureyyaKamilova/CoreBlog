c7f4c68ae21304933f620c2e0e8ac5062683576a
user <user@DESKTOP-DDOO6UB>
2024-05-01 13:25:07 +0400

    Add BlogListDashBoard

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashBoard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashBoard:ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EFBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = blogManager.GetBlogListCategory();
+            return View(values);
+        } 
+    }
+}
