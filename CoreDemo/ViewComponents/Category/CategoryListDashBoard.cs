b45e1fb839491c41cce5321480c2e964d3c3a13d
user <user@DESKTOP-DDOO6UB>
2024-05-01 13:46:25 +0400

    Add CategoryListDashBoard

---
+++ CoreDemo/ViewComponents/Category/CategoryListDashBoard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashBoard:ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
