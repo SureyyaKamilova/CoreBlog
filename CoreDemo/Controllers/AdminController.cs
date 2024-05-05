37a0555792b66654284cb61a7a08fd7c11ed18cc
user <user@DESKTOP-DDOO6UB>
2024-05-05 21:02:03 +0400

    Add AdminTema and Admin Controller

---
+++ CoreDemo/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminNavBarPartial()
+        {
+            return View();
+        }
+    }
+}
