f98b007fd30fbe2b228f53201bd6f43f66d17964
user <user@DESKTOP-DDOO6UB>
2024-05-07 18:43:25 +0400

    Add Category and Home controller to Areas

---
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryRepository());
+
+        public IActionResult Index()
+        {
+            var values = categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -54,7 +54,7 @@ namespace CoreDemo
                 options.SlidingExpiration = true;
             });
         }
-        
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -72,7 +72,7 @@ namespace CoreDemo
             app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
 
             //app.UseStatusCodePages();
-            
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -84,6 +84,11 @@ namespace CoreDemo
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
