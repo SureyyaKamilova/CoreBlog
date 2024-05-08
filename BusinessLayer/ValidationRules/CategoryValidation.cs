d145ae778b100d91205ccc6499e6a35a026efc4a
user <user@DESKTOP-DDOO6UB>
2024-05-08 16:17:45 +0400

    Add Category Controller

---
+++ BusinessLayer/ValidationRules/CategoryValidation.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidation:AbstractValidator<Category>
+    {
+        public CategoryValidation()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty()
+                                        .WithMessage("The category name will not be crossed");
+            RuleFor(x => x.CategoryDescription).NotEmpty()
+                                        .WithMessage("The category description will not be crossed");
+            RuleFor(x => x.CategoryName).MinimumLength(2)
+                                        .WithMessage("The category name must be more than the usual 2 characters");
+            RuleFor(x => x.CategoryName).MaximumLength(50)
+                                        .WithMessage("The category name should be less than 50 characters");
+            RuleFor(x => x.CategoryDescription).MinimumLength(8)
+                                        .WithMessage("The category description must be more than the usual 8 characters");
+
+        }
+    }
+}
--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFrameworks;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
@@ -9,11 +14,44 @@ namespace CoreDemo.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EFCategoryRepository());
-
+        Context context =new Context();
         public IActionResult Index(int page=1)
         {
             var values = categoryManager.GetList().ToPagedList(page, 3);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidation categoryValidation=new CategoryValidation();
+            ValidationResult validationResult=categoryValidation.Validate(category);
+
+            if (validationResult.IsValid)
+            {
+                category.CategoryStatus = true;
+                categoryManager.TAdd(category);
+
+                return RedirectToAction("Index", "Category");
+
+            }
+            else
+            {
+                foreach(var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                
+            }
+            return View();
+
+
         }
     }
 }
