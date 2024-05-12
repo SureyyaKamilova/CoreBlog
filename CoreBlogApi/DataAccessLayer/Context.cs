3444b04bcd74f9da1579bfe8b16a5b1e2e3a09ad
user <user@DESKTOP-DDOO6UB>
2024-05-12 17:41:24 +0400

    Add new database:CoreBlogApi

---
+++ CoreBlogApi/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreBlogApi.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost; database=CoreBlogApi; integrated security=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CoreBlogApi/DataAccessLayer/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreBlogApi.DataAccessLayer
+{
+    public class Employee
+    {
+        [Key]
+        public int Id {  get; set; }
+        public string Name { get; set; }
+    }
+}
