24f1dfbd4549eff639d26816879dc003d3a0d1db
user <user@DESKTOP-DDOO6UB>
2024-05-12 13:39:13 +0400

    Create Index with Ajax

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,44 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            {
+                categoryname = "Texnologiya",
+                categorycount = 10
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Proqramlaşdırma",
+                categorycount = 14
+            }); 
+            list.Add(new CategoryClass
+            {
+                categoryname = "İdman",
+                categorycount = 5
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Film və serial",
+                categorycount = 8
+            });
+
+            return Json(new { jsonlist = list });
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,53 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterById(int id)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == id);
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            
+            return Json(jsonWriters);
+
+        }
+
+        public static List<WriterModel> writers = new List<WriterModel>
+        {
+            new WriterModel
+            {
+                Id=1,
+                Name="Sürəyya",
+            },
+            new WriterModel{
+                Id=2,
+                Name="Gülşən",
+            },
+            new WriterModel{
+                Id=3,
+                Name="Leyla",
+            },
+            new WriterModel{
+                Id=4,
+                Name="Fəridə",
+            }
+        };
+    }
+}
