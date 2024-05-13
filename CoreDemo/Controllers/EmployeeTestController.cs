580dd3c42de8c82dbe57bd7857caa165528f1e47
user <user@DESKTOP-DDOO6UB>
2024-05-13 15:44:53 +0400

    Test commit

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async  Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("");
+
+            var jsonString=await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+
+            return View(values);
+        }
+
+        public class Class1
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
