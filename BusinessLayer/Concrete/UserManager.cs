327641d32c7fec007267f6ca2628560a79a78f4c
user <user@DESKTOP-DDOO6UB>
2024-05-23 17:06:27 +0400

    Add Identity to Writer page

---
+++ BusinessLayer/Concrete/UserManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Interfaces;
+using DataAccessLayer.Interfaces;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUser _user;
+        public UserManager(IUser user)
+        {
+            _user = user;
+        }
+
+        public AppUser GetById(int id)
+        {
+            return _user.GetById(id);
+        }
+
+        public List<AppUser> GetList()
+        {
+            return _user.GetAll();
+        }
+
+        public void TAdd(AppUser user)
+        {
+            _user.Insert(user);
+        }
+
+        public void TDelete(AppUser user)
+        {
+            _user.Delete(user);
+        }
+
+        public void TUpdate(AppUser user)
+        {
+            _user.Update(user);
+        }
+    }
+}
--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -6,6 +6,7 @@ using DataAccessLayer.EntityFrameworks;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
@@ -14,45 +15,52 @@ using System.Linq;
 namespace CoreDemo.Controllers
 {
     public class WriterController : Controller
-	{
-		WriterManager writerManager = new WriterManager(new EFWriterRepository());
+    {
+        WriterManager writerManager = new WriterManager(new EFWriterRepository());
         Context context = new Context();
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public WriterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         [Authorize]
         public IActionResult Index()
-		{
-			var userMail = User.Identity.Name;
-			ViewBag.v = userMail;
+        {
+            var userMail = User.Identity.Name;
+            ViewBag.v = userMail;
 
-			var writerName = context.Writers
-				.Where(x => x.WriterMail == userMail)
-				.Select(y => y.WriterName)
-				.FirstOrDefault();
-			ViewBag.v2 = writerName;
-			return View();
-		}
+            var writerName = context.Writers
+                .Where(x => x.WriterMail == userMail)
+                .Select(y => y.WriterName)
+                .FirstOrDefault();
+            ViewBag.v2 = writerName;
+            return View();
+        }
 
-		public IActionResult WriterProfile()
-		{
-			return View();
-		}
-		
-		public IActionResult WriterMail()
-		{
-			return View();
-		}
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
 
-		[AllowAnonymous]
-		public IActionResult Test()
-		{
-			return View();
-		}
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
 
-		[AllowAnonymous]
-		public PartialViewResult WriterNavBarPartial()
-		{
-			return PartialView();
-		}
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterNavBarPartial()
+        {
+            return PartialView();
+        }
 
         [AllowAnonymous]
         public PartialViewResult WriterHeaderPartial()
@@ -60,76 +68,81 @@ namespace CoreDemo.Controllers
             return PartialView();
         }
 
-		[AllowAnonymous]
-		[HttpGet]
-		public IActionResult WriterEditProfile()
-		{
-			var userName = User.Identity.Name;
-			var userMail = context.Users.Where(x => x.UserName == userName)
-										.Select(y => y.Email)                
-				                        .FirstOrDefault();
-			var writerId = context.Writers.Where(x => x.WriterMail == userMail)
-								 .Select(y => y.WriterId).FirstOrDefault();
-			var writerValues = writerManager.GetById(writerId);
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
 
-			return View(writerValues);
-		}
+            UserManager userManager = new UserManager(new EFUserRepository());
 
-		[HttpPost]
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email)
+                                .FirstOrDefault();
+            //var values = await _userManager.FindByNameAsync(userName);
+            var id = context.Users.Where(x => x.Email == userMail)
+                .Select(y => y.Id).FirstOrDefault();
+
+            var values = userManager.GetById(id);
+
+            return View(values);
+
+        }
+
+        [HttpPost]
         public IActionResult WriterEditProfile(Writer writer)
         {
-			WriterValidation writerValidation = new WriterValidation();
-			ValidationResult validationResult = writerValidation.Validate(writer);
+            WriterValidation writerValidation = new WriterValidation();
+            ValidationResult validationResult = writerValidation.Validate(writer);
 
             if (validationResult.IsValid)
-			{
-				writerManager.TUpdate(writer);
-				return RedirectToAction("Index","DashBoard");
-			}
-			else
-			{
-				foreach(var item in validationResult.Errors)
-				{
-					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-				}
-			}
+            {
+                writerManager.TUpdate(writer);
+                return RedirectToAction("Index", "DashBoard");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
 
-			return View();
-		}
+            return View();
+        }
 
-		[AllowAnonymous]
-		[HttpGet]
-		public IActionResult WriterAdd()
-		{
-			return View();
-		}
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
 
-		[AllowAnonymous]
-		[HttpPost]
-		public IActionResult WriterAdd(AddProfileImage addProfileImage)
-		{
-			Writer writer = new Writer();
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage addProfileImage)
+        {
+            Writer writer = new Writer();
 
-			if (addProfileImage.WriterImage != null)
-			{
-				var extension = Path.GetExtension(addProfileImage.WriterImage.FileName);
-				var newImageName = Guid.NewGuid() + extension;
-				var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/CoreBlogTema/WriterImageFiles/", newImageName);
-				var stream = new FileStream(location, FileMode.Create);
-				
-				addProfileImage.WriterImage.CopyTo(stream);
-				writer.WriterImage = newImageName;
+            if (addProfileImage.WriterImage != null)
+            {
+                var extension = Path.GetExtension(addProfileImage.WriterImage.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/CoreBlogTema/WriterImageFiles/", newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+
+                addProfileImage.WriterImage.CopyTo(stream);
+                writer.WriterImage = newImageName;
             }
 
             writer.WriterName = addProfileImage.WriterName;
             writer.WriterMail = addProfileImage.WriterMail;
-			writer.WriterPassword = addProfileImage.WriterPassword;
-			writer.WriterStatus = true;
-			writer.WriterAbout=addProfileImage.WriterAbout;
+            writer.WriterPassword = addProfileImage.WriterPassword;
+            writer.WriterStatus = true;
+            writer.WriterAbout = addProfileImage.WriterAbout;
 
-			writerManager.TAdd(writer);
-			return RedirectToAction("Index", "DashBoard");
-		}
+            writerManager.TAdd(writer);
+            return RedirectToAction("Index", "DashBoard");
+        }
 
     }
 
