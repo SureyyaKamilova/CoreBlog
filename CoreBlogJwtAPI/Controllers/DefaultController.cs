52cc7027983f9debc8310ee64758ceb89d22e8a2
user <user@DESKTOP-DDOO6UB>
2024-06-10 19:29:50 +0400

    Add JWT Bearer

---
+++ CoreBlogJwtAPI/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using CoreBlogJwtAPI.DataAccessLayer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlogJwtAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [HttpGet("[action]")]
+        public IActionResult LogIn()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult PageOne()
+        {
+            return Ok("Page one login was successful");
+        }
+
+    }
+}
